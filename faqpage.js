747a4bf84e56e766bb5b0213f87440f1ec42e193
Sayali098 <sayalishelake9022@gmail.com>
2022-11-29 19:42:19 +0530

    faqpage js file added

---
+++ faqpage.js
@@ -0,0 +1,8 @@
+var accordion=document.getElementsByClassName('contentbox');
+
+for(i=0; i<accordion.length; i++)
+{
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+}
